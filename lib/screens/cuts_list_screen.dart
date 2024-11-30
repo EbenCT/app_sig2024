8b91e513ebe77a7d16dbab4254c34af3d88edcc6
EbenCT <cayoeben64@gmail.com>
2024-11-29 20:38:10 -0400

    Mapa implementado (faltan funcionalidades)

--- lib/screens/cuts_list_screen.dart
+++ lib/screens/cuts_list_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/cut.dart';
 import '../providers/cuts_provider.dart';
 
 class CutsListScreen extends StatelessWidget {
